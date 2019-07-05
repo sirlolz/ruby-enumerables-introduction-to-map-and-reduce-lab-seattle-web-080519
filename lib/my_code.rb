a1188f7e10ff45c0d705efc227c547f37ae60b15
Jared Burnham <zlolris97@gmail.com>
2019-07-05 20:34:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -49,7 +49,7 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-  if source_array[0]..source_array[source_array.length]
+  if source_array[0]..source_array[source_array.length] == true
     return true
   else return false
   end
